b65f161df145f85eb44dde2b7247a7f03a57ef84
Medido1 <medi1222@gmail.com>
2024-11-04 22:16:14 +0100

    Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import LinkedinIcon from '../assets/linkedin.png';
+
+function Footer() {
+  return (
+    <div className='bg-black flex justify-between items-center p-4'>
+      <a 
+        href="https://www.linkedin.com/in/sara-elalia-mouloudj-b5a196252/"
+        target='blank'>
+        <img 
+          src={LinkedinIcon} 
+          alt="linkedIn profile"
+          className='w-6 h-auto'/>
+      </a>
+      
+      <p className='text-white'>
+        Bab Dzair -Blida - Algeria
+      </p>
+    </div>
+  )
+}
+
+export default Footer;
